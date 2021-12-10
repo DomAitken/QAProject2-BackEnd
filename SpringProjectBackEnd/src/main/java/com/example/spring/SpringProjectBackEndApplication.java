786bc03033c4a3d59c7b8769d6fa4a84e5d7bd73
DomAitken <93253286+DomAitken@users.noreply.github.com>
2021-12-10 15:12:34 +0000

    Back End Application - Java File

---
+++ SpringProjectBackEnd/src/main/java/com/example/spring/SpringProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectBackEndApplication.class, args);
+	}
+
+}
